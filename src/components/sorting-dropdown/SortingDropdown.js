58b67b83875413193fe69fb5381b8c5cbcddd944
varunC28 <85437818+varunC28@users.noreply.github.com>
2025-07-07 05:51:31 +0530

    Fix CSS issues: sorting, product spacing, address page, and product detail styling

--- src/components/sorting-dropdown/SortingDropdown.js
+++ src/components/sorting-dropdown/SortingDropdown.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import { MenuItem, Select, FormControl, InputLabel } from '@mui/material';
 import './SortingDropdown.css';
 
-const SortingDropdown = ({ onSortChange }) => {
+const SortingDropdown = ({ onSort }) => {
   const [sortBy, setSortBy] = useState('default');
 
   const handleSortChange = (event) => {
     const newSortBy = event.target.value;
     setSortBy(newSortBy);
-    onSortChange(newSortBy);
+    onSort(newSortBy);
   };
 
   return (
@@ -22,9 +22,10 @@ const SortingDropdown = ({ onSortChange }) => {
         onChange={handleSortChange}
       >
         <MenuItem value="default">Default</MenuItem>
-        <MenuItem value="priceHighToLow">Price High to Low</MenuItem>
-        <MenuItem value="priceLowToHigh">Price Low to High</MenuItem>
-        <MenuItem value="newest">Newest</MenuItem>
+        <MenuItem value="price-high-to-low">Price High to Low</MenuItem>
+        <MenuItem value="price-low-to-high">Price Low to High</MenuItem>
+        <MenuItem value="name-a-to-z">Name A to Z</MenuItem>
+        <MenuItem value="name-z-to-a">Name Z to A</MenuItem>
       </Select>
     </FormControl>
     </div>
